a94f0c065d7a27be8f07bd3563b243bbd43f60e9
Hristo Petkov <45469690+hristopetkovv@users.noreply.github.com>
2023-02-04 17:37:23 +0200

    Data: create log database

---
+++ BookStore/BookStore.Persistence/Migrations/20230204153339_CreateLogContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace BookStore.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateLogContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "log",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    type = table.Column<int>(type: "integer", nullable: false),
+                    logdate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ip = table.Column<string>(type: "text", nullable: true),
+                    verb = table.Column<string>(type: "text", nullable: true),
+                    url = table.Column<string>(type: "text", nullable: true),
+                    useragent = table.Column<string>(type: "text", nullable: true),
+                    userid = table.Column<int>(type: "integer", nullable: true),
+                    message = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_log", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "log");
+        }
+    }
+}
